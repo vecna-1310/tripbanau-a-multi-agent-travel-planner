a37c66531af57bfd3a916b636b93ac098130c7be
vecna-1310 <you@example.com>
2026-09-11 23:38:43 +0530

    app added

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 from tools.tavily_tool import tavily_search
 
 from tools.flight_tool import search_flights
-
+from backend import run_travel_agent
 # res = tavily_search("Best hotels in India") 
 # print(res)
 
-flight_res = search_flights("Plan a 7 days Japan trip from INDIA")
-print(flight_res)
+# flight_res = search_flights("Plan a 7 days Japan trip from INDIA")
+# print(flight_res)
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
